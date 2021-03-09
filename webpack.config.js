c7b87f69ac94af93b6ef97ac28280e0392c42a94
Ronaldo Ferreyra <ronaldoferreyra@yahoo.com.br>
2021-03-09 08:54:53 -0300

    Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -11,6 +10,9 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
     },
     plugins: [
         new HtmlWebpackPlugin({
