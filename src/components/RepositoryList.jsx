312b8c61b54a908a22a3cf099572ea0b62aca13d
Ronaldo Ferreyra <ronaldoferreyra@yahoo.com.br>
2021-03-09 22:03:53 -0300

    Primeiro componente React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,52 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list"> 
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Fomrs in React</p>
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
